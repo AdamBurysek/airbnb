e4a9ad33117ac5a55243deb14ad792ca47d716e3
AdamBurysek <adam.burysek@seznam.cz>
2023-11-19 22:07:06 +0100

    add listing and data

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,29 @@
 import { View, Text } from "react-native";
-import React from "react";
+import React, { useMemo, useState } from "react";
 import { Link } from "expo-router";
+import { Stack } from "expo-router";
+import ExporeHeader from "@/components/exporeHeader";
+import Listings from "@/components/listings";
+import listingsData from "@/assets/data/airbnb-listings.json";
 
 const Page = () => {
+  const [category, setCategory] = useState("Tiny homes");
+  const items = useMemo(() => listingsData as any, []);
+
+  const onDataChanged = (category: string) => {
+    setCategory(category);
+  };
+
   return (
-    <View>
-      <Link href={"/(modals)/login"}>Login</Link>
-      <Link href={"/(modals)/booking"}>Bookings</Link>
-      <Link href={"/listing/1337"}>Listing</Link>
+    <View style={{ flex: 1, marginTop: 130 }}>
+      <Stack.Screen
+        options={{
+          header: () => (
+            <ExporeHeader onCategoryChanged={onDataChanged}></ExporeHeader>
+          ),
+        }}
+      />
+      <Listings listings={items} category={category}></Listings>
     </View>
   );
 };
