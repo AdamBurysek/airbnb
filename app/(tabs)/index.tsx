a6da2952c72275f2c50506b6110bbe1e8ceedfe6
AdamBurysek <adam.burysek@seznam.cz>
2023-11-20 21:58:22 +0100

    add map

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,10 +5,13 @@ import { Stack } from "expo-router";
 import ExporeHeader from "@/components/exporeHeader";
 import Listings from "@/components/listings";
 import listingsData from "@/assets/data/airbnb-listings.json";
+import listingsMaps from "@/assets/data/airbnb-listings.geo.json";
+import ListingsMap from "@/components/listingsMap";
 
 const Page = () => {
   const [category, setCategory] = useState("Tiny homes");
   const items = useMemo(() => listingsData as any, []);
+  const mapItems = useMemo(() => listingsMaps as any, []);
 
   const onDataChanged = (category: string) => {
     setCategory(category);
@@ -23,7 +26,8 @@ const Page = () => {
           ),
         }}
       />
-      <Listings listings={items} category={category}></Listings>
+      {/* <Listings listings={items} category={category}></Listings> */}
+      <ListingsMap listings={mapItems}></ListingsMap>
     </View>
   );
 };
---
+++ components/listingsMap.tsx
@@ -0,0 +1,109 @@
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
+import { Marker, PROVIDER_GOOGLE } from "react-native-maps";
+import MapView from "react-native-map-clustering";
+import { defaultStyles } from "@/constants/Styles";
+import { useRouter } from "expo-router";
+
+interface Props {
+  listings: any;
+}
+
+const INITIAL_REGION = {
+  latitude: 52.520008,
+  longitude: 13.404954,
+  latitudeDelta: 9,
+  longitudeDelta: 9,
+};
+
+const ListingsMap = ({ listings }: Props) => {
+  const router = useRouter();
+
+  const onMarkerSelected = (item: any) => {
+    router.push(`/listing/${item.properties.id}`);
+  };
+
+  const renderCluster = (cluster: any) => {
+    const { id, geometry, onPress, properties } = cluster;
+
+    const points = properties.point_count;
+    return (
+      <Marker
+        key={`cluster-${id}`}
+        coordinate={{
+          longitude: geometry.coordinates[0],
+          latitude: geometry.coordinates[1],
+        }}
+        onPress={onPress}
+      >
+        <View style={styles.marker}>
+          <Text
+            style={{
+              color: "#000",
+              textAlign: "center",
+              fontFamily: "mon-sb",
+            }}
+          >
+            {points}
+          </Text>
+        </View>
+      </Marker>
+    );
+  };
+
+  return (
+    <View style={defaultStyles.container}>
+      <MapView
+        animationEnabled={false}
+        style={StyleSheet.absoluteFill}
+        provider={PROVIDER_GOOGLE}
+        showsUserLocation
+        showsMyLocationButton
+        initialRegion={INITIAL_REGION}
+        clusterColor="white"
+        clusterTextColor="black"
+        clusterFontFamily="mon-sb"
+        renderCluster={renderCluster}
+      >
+        {listings.features.map((item: any) => (
+          <Marker
+            key={item.properties.id}
+            onPress={() => onMarkerSelected(item)}
+            coordinate={{
+              latitude: +item.properties.latitude,
+              longitude: +item.properties.longitude,
+            }}
+          >
+            <View style={styles.marker}>
+              <Text style={styles.markerText}>€ {item.properties.price}</Text>
+            </View>
+          </Marker>
+        ))}
+      </MapView>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  marker: {
+    padding: 8,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#fff",
+    elevation: 5,
+    borderRadius: 12,
+    shadowColor: "#000",
+    shadowOpacity: 0.1,
+    shadowRadius: 6,
+    shadowOffset: {
+      width: 1,
+      height: 10,
+    },
+  },
+  markerText: {
+    fontSize: 14,
+    fontFamily: "mon-sb",
+  },
+});
+
+export default ListingsMap;
